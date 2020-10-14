20bb11b3dbcfbc7c7214e14627cf12f1ce7312c8
genesisporras <genesis.porras777@gmail.com>
2020-10-14 19:57:33 -0300

    tarea v2.0

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -94,18 +94,15 @@ function esPrimo(numero) {
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
 
-  if (numero === 0 || numero === 1){
+  if (numero === 0 || numero === 1){    
     return false;
   }
-  for (var i = 2 ; numero%i===0; i++){
-    if (numero !== i){
+  for (var i = 2 ; i<numero; i++){    
+    if (numero % i===0){      
       return false;
     } 
   }
   return true;
- 
-     
-   
 }
 
 
